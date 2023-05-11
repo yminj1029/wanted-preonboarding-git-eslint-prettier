cb49f58d1cd099f42aee9cbaf874e5e23a8b1b21
MinjiYun <minjiyun204@gmail.com>
2023-05-11 17:59:28 +0900

    fix: delete unused value

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const test = () => {
-    console.log('hello, world??');
-  };
   return (
     <div className="App">
       <header className="App-header">
